ea5c215b14fd336197340e259f9a969a37e738de
Jordan Moore <moorej@avalonconsult.com>
2017-02-26 20:28:36 -0600

    Update apache-spark profile

--- profile.d/apache-spark.sh
+++ profile.d/apache-spark.sh
@@ -1,8 +1,17 @@
-SPARK_VERSION=`ls /usr/local/Cellar/apache-spark/ | sort | tail -1`
+
+ROOTDIR=$CASKROOM_HOME
+if [ -z ${ROOTDIR-x} ]; then 
+    ROOTDIR=/opt 
+    echo "Profile error: ROOTDIR not defined. Setting to $ROOTDIR"
+fi
+
+SPARK_FOLDER=apache-spark16
+SPARK_VERSION=`ls ${ROOTDIR}/${SPARK_FOLDER}/ | sort | tail -1`
 if which pyspark > /dev/null; then
-  export SPARK_HOME="/usr/local/Cellar/apache-spark/$SPARK_VERSION/libexec"
+  export SPARK_HOME="${ROOTDIR}/$SPARK_FOLDER/$SPARK_VERSION"
   # export PYSPARK_SUBMIT_ARGS="--master local[2]"
   # Add the PySpark classes to the Python path:
   export PYTHONPATH="${SPARK_HOME}/python/:$PYTHONPATH"
-  export PYTHONPATH="${SPARK_HOME}/python/lib/py4j-0.9-src.zip:$PYTHONPATH"
+  py4j=$(find $SPARK_HOME -name 'py4j*.zip')
+  export PYTHONPATH="$py4j:$PYTHONPATH"
 fi
