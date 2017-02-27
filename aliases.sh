736cfc6f7e372694628fbc5160ad9cb42008616e
Jordan Moore <moorej@avalonconsult.com>
2017-02-26 20:25:11 -0600

    Add pip_upgrade aliases

--- aliases.sh
+++ aliases.sh
@@ -3,3 +3,6 @@ alias vup='vagrant up'
 alias vdo='vagrant halt'
 alias vssh='vagrant ssh'
 alias vkill='vagrant destroy'
+
+alias upgrade_pip2_packages="pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs pip2 install -U"
+alias upgrade_pip3_packages="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs pip3 install -U"
