0019599fdf1db0b3e8acfcd496e72fdedcaa4a87
Jordan Moore <crikket.007@gmail.com>
2017-03-13 01:25:22 -0500

    Update apache-spark.sh

--- profile.d/apache-spark.sh
+++ profile.d/apache-spark.sh
@@ -1,17 +1,20 @@
 
-ROOTDIR=$CASKROOM_HOME
+ROOTDIR=/usr/local/Cellar
 if [ -z ${ROOTDIR-x} ]; then 
-    ROOTDIR=/opt 
+    ROOTDIR=/opt
     echo "Profile error: ROOTDIR not defined. Setting to $ROOTDIR"
 fi
 
-SPARK_FOLDER=apache-spark16
-SPARK_VERSION=`ls ${ROOTDIR}/${SPARK_FOLDER}/ | sort | tail -1`
-if which pyspark > /dev/null; then
-  export SPARK_HOME="${ROOTDIR}/$SPARK_FOLDER/$SPARK_VERSION"
+SPARK_FOLDER=apache-spark@1.6
+SPARK_VERSION=`ls ${ROOTDIR}/$SPARK_FOLDER | sort | tail -1`
+export SPARK_HOME="${ROOTDIR}/$SPARK_FOLDER"
+# When using Homebrew, go into libexec
+export SPARK_HOME="$SPARK_HOME/$SPARK_VERSION/libexec"
+export PATH="${PATH}:$SPARK_HOME/bin"
+
+if which pyspark > /dev/null; then 
   # export PYSPARK_SUBMIT_ARGS="--master local[2]"
   # Add the PySpark classes to the Python path:
-  export PYTHONPATH="${SPARK_HOME}/python/:$PYTHONPATH"
-  py4j=$(find $SPARK_HOME -name 'py4j*.zip')
-  export PYTHONPATH="$py4j:$PYTHONPATH"
+  export PYTHONPATH="$SPARK_HOME/python/:$PYTHONPATH"
+  export PYTHONPATH="$(find $SPARK_HOME/python/lib -name 'py4j*.zip'):$PYTHONPATH"
 fi
